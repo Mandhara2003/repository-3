acca4218567d315bfb0d67e099b046a59b33732d
Mandhara2003 <115629360+Mandhara2003@users.noreply.github.com>
2022-12-26 20:16:22 +0530

    Prime.c

---
+++ Prime.c
@@ -0,0 +1,23 @@
+Import java.util.scanner;
+Public class prime
+{
+Public static void main (string []args);
+{
+Int i,n,flag=0;
+System.out.println("enter the number\n");
+Scanner in=new Scanner(System.in);
+n=nextInt();
+{
+for(i=2;i<n;i++);
+If(n%==0)
+Flag=1;
+break;
+}
+{
+If(flag==1);
+System.out.println("lt is not a prime");
+Else
+System.out.println("It is a prime");
+}
+}
+}
